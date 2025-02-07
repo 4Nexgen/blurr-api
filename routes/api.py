99f4b0b4d5d6d4c8e78aa3b6f1480ae452052a41
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2025-02-07 11:38:49 +0800

    removed users and auth in the meantime

--- routes/api.py
+++ routes/api.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
-from src.endpoints.users import UsersEndpoint
-from src.endpoints.auth import AuthEndpoint
+# from src.endpoints.users import UsersEndpoint
+# from src.endpoints.auth import AuthEndpoint
 from src.endpoints.cli import CLIEndpoint
 
 APIRoutes = APIRouter()
 
 class Routes:
     def load_routes(self):
-        APIRoutes.include_router(UsersEndpoint, prefix="/api/users", tags=["Users"])
-        APIRoutes.include_router(AuthEndpoint, prefix="/api/auth", tags=["Authentication"])
+        # APIRoutes.include_router(UsersEndpoint, prefix="/api/users", tags=["Users"])
+        # APIRoutes.include_router(AuthEndpoint, prefix="/api/auth", tags=["Authentication"])
         APIRoutes.include_router(CLIEndpoint, prefix="/api/cli", tags=["CLI"])
